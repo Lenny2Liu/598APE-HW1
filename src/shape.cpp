c8842790c2fc875f2e471d9e181607bd734f74f4
Lenny2Liu <117865161+Lenny2Liu@users.noreply.github.com>
2025-02-14 12:34:19 -0600

    bvh

--- src/shape.cpp
+++ src/shape.cpp
@@ -52,68 +52,68 @@ void insertionSort(TimeAndShape *arr, int n) {
     }
 }
 
-void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
-   ShapeNode* t = c->listStart;
-   TimeAndShape *times = (TimeAndShape*)malloc(0);
-   size_t seen = 0;
-   while(t!=NULL){
-      double time = t->data->getIntersection(ray);
+// void calcColor(unsigned char* toFill,Autonoma* c, Ray ray, unsigned int depth){
+//    ShapeNode* t = c->listStart;
+//    TimeAndShape *times = (TimeAndShape*)malloc(0);
+//    size_t seen = 0;
+//    while(t!=NULL){
+//       double time = t->data->getIntersection(ray);
 
-      TimeAndShape *times2 = (TimeAndShape*)malloc(sizeof(TimeAndShape)*(seen + 1));
-      for (int i=0; i<seen; i++)
-         times2[i] = times[i];
-      times2[seen] = (TimeAndShape){ time, t->data };
-      free(times);
-      times = times2;
-      seen ++;
-      t = t->next;
-   }
-   insertionSort(times, seen);
-   if (seen == 0 || times[0].time == inf) {
-      double opacity, reflection, ambient;
-      Vector temp = ray.vector.normalize();
-      const double x = temp.x;
-      const double z = temp.z;
-      const double me = (temp.y<0)?-temp.y:temp.y;
-      const double angle = atan2(z, x);
-      c->skybox->getColor(toFill, &ambient, &opacity, &reflection, fix(angle/M_TWO_PI),fix(me));
-      return;
-   }
+//       TimeAndShape *times2 = (TimeAndShape*)malloc(sizeof(TimeAndShape)*(seen + 1));
+//       for (int i=0; i<seen; i++)
+//          times2[i] = times[i];
+//       times2[seen] = (TimeAndShape){ time, t->data };
+//       free(times);
+//       times = times2;
+//       seen ++;
+//       t = t->next;
+//    }
+//    insertionSort(times, seen);
+//    if (seen == 0 || times[0].time == inf) {
+//       double opacity, reflection, ambient;
+//       Vector temp = ray.vector.normalize();
+//       const double x = temp.x;
+//       const double z = temp.z;
+//       const double me = (temp.y<0)?-temp.y:temp.y;
+//       const double angle = atan2(z, x);
+//       c->skybox->getColor(toFill, &ambient, &opacity, &reflection, fix(angle/M_TWO_PI),fix(me));
+//       return;
+//    }
 
-   double curTime = times[0].time;
-   Shape* curShape = times[0].shape;
-   free(times);
+//    double curTime = times[0].time;
+//    Shape* curShape = times[0].shape;
+//    free(times);
 
-   Vector intersect = curTime*ray.vector+ray.point;
-   double opacity, reflection, ambient;
-   curShape->getColor(toFill, &ambient, &opacity, &reflection, c, Ray(intersect, ray.vector), depth);
+//    Vector intersect = curTime*ray.vector+ray.point;
+//    double opacity, reflection, ambient;
+//    curShape->getColor(toFill, &ambient, &opacity, &reflection, c, Ray(intersect, ray.vector), depth);
    
-   double lightData[3];
-   getLight(lightData, c, intersect, curShape->getNormal(intersect), curShape->reversible());
-   toFill[0] = (unsigned char)(toFill[0]*(ambient+lightData[0]*(1-ambient)));
-   toFill[1] = (unsigned char)(toFill[1]*(ambient+lightData[1]*(1-ambient)));
-   toFill[2] = (unsigned char)(toFill[2]*(ambient+lightData[2]*(1-ambient)));
-   if(depth<c->depth && (opacity<1-1e-6 || reflection>1e-6)){
-      unsigned char col[4];
-      if(opacity<1-1e-6){
-         Ray nextRay = Ray(intersect+ray.vector*1E-4, ray.vector);
-         calcColor(col, c, nextRay, depth+1);
-         toFill[0]= (unsigned char)(toFill[0]*opacity+col[0]*(1-opacity));
-         toFill[1]= (unsigned char)(toFill[1]*opacity+col[1]*(1-opacity));
-         toFill[2]= (unsigned char)(toFill[2]*opacity+col[2]*(1-opacity));        
-      }
-      if(reflection>1e-6){
-         Vector norm = curShape->getNormal(intersect).normalize();
-         Vector vec = ray.vector-2*norm*(norm.dot(ray.vector));
-         Ray nextRay = Ray(intersect+vec*1E-4, vec);
-         calcColor(col, c, nextRay, depth+1);
+//    double lightData[3];
+//    getLight(lightData, c, intersect, curShape->getNormal(intersect), curShape->reversible());
+//    toFill[0] = (unsigned char)(toFill[0]*(ambient+lightData[0]*(1-ambient)));
+//    toFill[1] = (unsigned char)(toFill[1]*(ambient+lightData[1]*(1-ambient)));
+//    toFill[2] = (unsigned char)(toFill[2]*(ambient+lightData[2]*(1-ambient)));
+//    if(depth<c->depth && (opacity<1-1e-6 || reflection>1e-6)){
+//       unsigned char col[4];
+//       if(opacity<1-1e-6){
+//          Ray nextRay = Ray(intersect+ray.vector*1E-4, ray.vector);
+//          calcColor(col, c, nextRay, depth+1);
+//          toFill[0]= (unsigned char)(toFill[0]*opacity+col[0]*(1-opacity));
+//          toFill[1]= (unsigned char)(toFill[1]*opacity+col[1]*(1-opacity));
+//          toFill[2]= (unsigned char)(toFill[2]*opacity+col[2]*(1-opacity));        
+//       }
+//       if(reflection>1e-6){
+//          Vector norm = curShape->getNormal(intersect).normalize();
+//          Vector vec = ray.vector-2*norm*(norm.dot(ray.vector));
+//          Ray nextRay = Ray(intersect+vec*1E-4, vec);
+//          calcColor(col, c, nextRay, depth+1);
       
-         toFill[0]= (unsigned char)(toFill[0]*(1-reflection)+col[0]*(reflection));
-         toFill[1]= (unsigned char)(toFill[1]*(1-reflection)+col[1]*(reflection));
-         toFill[2]= (unsigned char)(toFill[2]*(1-reflection)+col[2]*(reflection));
-      }
-   }
-}
+//          toFill[0]= (unsigned char)(toFill[0]*(1-reflection)+col[0]*(reflection));
+//          toFill[1]= (unsigned char)(toFill[1]*(1-reflection)+col[1]*(reflection));
+//          toFill[2]= (unsigned char)(toFill[2]*(1-reflection)+col[2]*(reflection));
+//       }
+//    }
+// }
 
 
 inline bool intersectAABB(const AABB& box, const Ray& ray) {
@@ -226,62 +226,62 @@ bool intersectBVH(const BVHNode* node, const Ray& ray, double& outT, Shape*& out
 }
 
 
-// void calcColor(unsigned char* toFill, Autonoma* c, const Ray ray, unsigned int depth)
-// {
-//     double closestT = inf;
-//     Shape* closestShape = nullptr;
-//     bool hit = intersectBVH(c->bvhRoot, ray, closestT, closestShape, c->shapes);
-//     if(!hit || closestT == std::numeric_limits<double>::infinity()) {
+void calcColor(unsigned char* toFill, Autonoma* c, const Ray ray, unsigned int depth)
+{
+    double closestT = inf;
+    Shape* closestShape = nullptr;
+    bool hit = intersectBVH(c->bvhRoot, ray, closestT, closestShape, c->shapes);
+    if(!hit || closestT == std::numeric_limits<double>::infinity()) {
 
-//         Vector temp = ray.vector;
-//         temp.normalize();
-//         double x = temp.x, z = temp.z;
-//         double me = (temp.y < 0.0)? -temp.y : temp.y;
-//         double angle = atan2(z, x);
+        Vector temp = ray.vector;
+        temp.normalize();
+        double x = temp.x, z = temp.z;
+        double me = (temp.y < 0.0)? -temp.y : temp.y;
+        double angle = atan2(z, x);
 
-//         double opacity, reflection, ambient;
-//         c->skybox->getColor(toFill, &ambient, &opacity, &reflection,
-//                             fix(angle/(2.0 * M_PI)),
-//                             fix(me));
-//         return;
-//     }
+        double opacity, reflection, ambient;
+        c->skybox->getColor(toFill, &ambient, &opacity, &reflection,
+                            fix(angle/(2.0 * M_PI)),
+                            fix(me));
+        return;
+    }
 
-//     Vector intersect =  closestT * ray.vector + ray.point;
-//     double opacity, reflection, ambient;
-//     closestShape->getColor(toFill, &ambient, &opacity, &reflection,
-//                            c, Ray(intersect, ray.vector),
-//                            depth);
+    Vector intersect =  closestT * ray.vector + ray.point;
+    double opacity, reflection, ambient;
+    closestShape->getColor(toFill, &ambient, &opacity, &reflection,
+                           c, Ray(intersect, ray.vector),
+                           depth);
 
-//     double lightData[3];
-//     {
-//         Vector normal = closestShape->getNormal(intersect);
-//         getLight(lightData, c, intersect, normal, closestShape->reversible());
-//     }
+    double lightData[3];
+    {
+        Vector normal = closestShape->getNormal(intersect);
+        getLight(lightData, c, intersect, normal, closestShape->reversible());
+    }
 
-//     toFill[0] = (unsigned char)( toFill[0] * (ambient + lightData[0]*(1 - ambient)) );
-//     toFill[1] = (unsigned char)( toFill[1] * (ambient + lightData[1]*(1 - ambient)) );
-//     toFill[2] = (unsigned char)( toFill[2] * (ambient + lightData[2]*(1 - ambient)) );
+    toFill[0] = (unsigned char)( toFill[0] * (ambient + lightData[0]*(1 - ambient)) );
+    toFill[1] = (unsigned char)( toFill[1] * (ambient + lightData[1]*(1 - ambient)) );
+    toFill[2] = (unsigned char)( toFill[2] * (ambient + lightData[2]*(1 - ambient)) );
 
-//     if(depth < c->depth && (opacity < 1.0 - 1e-6 || reflection > 1e-6)) {
-//         unsigned char col[4];
-//         if(opacity < 1.0 - 1e-6) {
-//             Ray nextRay(intersect + 1e-4 * ray.vector, ray.vector);
-//             calcColor(col, c, nextRay, depth+1);
-//             toFill[0] = (unsigned char)( toFill[0]*opacity + col[0]*(1 - opacity) );
-//             toFill[1] = (unsigned char)( toFill[1]*opacity + col[1]*(1 - opacity) );
-//             toFill[2] = (unsigned char)( toFill[2]*opacity + col[2]*(1 - opacity) );
-//         }
-//         if(reflection > 1e-6) {
-//             Vector normal = closestShape->getNormal(intersect).normalize();
-//             double dotVN   = normal.dot(ray.vector);
-//             Vector reflVec = - normal*(2.0*dotVN) + ray.vector;
+    if(depth < c->depth && (opacity < 1.0 - 1e-6 || reflection > 1e-6)) {
+        unsigned char col[4];
+        if(opacity < 1.0 - 1e-6) {
+            Ray nextRay(intersect + 1e-4 * ray.vector, ray.vector);
+            calcColor(col, c, nextRay, depth+1);
+            toFill[0] = (unsigned char)( toFill[0]*opacity + col[0]*(1 - opacity) );
+            toFill[1] = (unsigned char)( toFill[1]*opacity + col[1]*(1 - opacity) );
+            toFill[2] = (unsigned char)( toFill[2]*opacity + col[2]*(1 - opacity) );
+        }
+        if(reflection > 1e-6) {
+            Vector normal = closestShape->getNormal(intersect).normalize();
+            double dotVN   = normal.dot(ray.vector);
+            Vector reflVec = - normal*(2.0*dotVN) + ray.vector;
 
-//             Ray reflRay(intersect + reflVec*1e-4, reflVec);
-//             calcColor(col, c, reflRay, depth+1);
+            Ray reflRay(intersect + reflVec*1e-4, reflVec);
+            calcColor(col, c, reflRay, depth+1);
 
-//             toFill[0] = (unsigned char)( toFill[0]*(1 - reflection) + col[0]*reflection );
-//             toFill[1] = (unsigned char)( toFill[1]*(1 - reflection) + col[1]*reflection );
-//             toFill[2] = (unsigned char)( toFill[2]*(1 - reflection) + col[2]*reflection );
-//         }
-//     }
-// }
+            toFill[0] = (unsigned char)( toFill[0]*(1 - reflection) + col[0]*reflection );
+            toFill[1] = (unsigned char)( toFill[1]*(1 - reflection) + col[1]*reflection );
+            toFill[2] = (unsigned char)( toFill[2]*(1 - reflection) + col[2]*reflection );
+        }
+    }
+}
